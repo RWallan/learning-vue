eb215ed600167c394e839890a2b49e7f42f6b477
RWallan <3am.richardwallan@gmail.com>
2024-06-08 20:21:19 -0300

    feat: Implementing Vue Router

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div>
+        <p>Footer</p>   
+    </div>
+</template>
+
+
+<style scoped>
+    
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">
+        <img src="" id="logo" />
+      </RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/orders">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
